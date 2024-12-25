d602877edc0b99a01b17c4bd98d7ccc83d2d4117
Samaptech <sameer2308b@aptechgdn.net>
2024-12-25 17:06:02 +0500

    main

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Net.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult Facts()
+        {
+            return View();
+        }
+
+        public IActionResult Packages()
+        {
+            return View();
+        }
+
+        public IActionResult OurAnimals()
+        {
+            return View();
+        }
+
+        public IActionResult VisitingHours()
+        {
+            return View();
+        }
+
+
+        public IActionResult Testimonials()
+        {
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
